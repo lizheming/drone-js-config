80007ba1ec725720a96e4f235fa32e069954ca6a
lizheming <i@imnerd.org>
2019-02-14 15:30:34 +0800

    fix http-signature not support signature header

--- middleware/signature.js
+++ middleware/signature.js
@@ -2,6 +2,11 @@ const httpSignature = require('http-signature');
 
 module.exports = function (secret) {
   return function signature(ctx, next) {
+    const { signature, authorization } = ctx.request.header;
+    if (signature && !authorization) {
+      ctx.request.header.authorization = `Signature ${signature}`;
+    }
+
     const parsed = httpSignature.parseRequest(ctx.req);
     if (!httpSignature.verifyHMAC(parsed, secret)) {
       return ctx.throw(401);
