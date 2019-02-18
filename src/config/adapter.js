991932bfac9e68d4a1e82e5018fbbe1d7c25ce3c
lizheming <i@imnerd.org>
2019-02-18 21:30:42 +0800

    thinkjs init

---
+++ src/config/adapter.js
@@ -0,0 +1,22 @@
+const { Console, DateFile } = require('think-logger3');
+const path = require('path');
+const isDev = think.env === 'development';
+
+/**
+ * logger adapter config
+ * @type {Object}
+ */
+exports.logger = {
+  type: isDev ? 'console' : 'dateFile',
+  console: {
+    handle: Console
+  },
+  dateFile: {
+    handle: DateFile,
+    level: 'ALL',
+    absolute: true,
+    pattern: '-yyyy-MM-dd',
+    alwaysIncludePattern: true,
+    filename: path.join(think.ROOT_PATH, 'logs/app.log')
+  }
+};
---
+++ src/config/middleware.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const isDev = think.env === 'development';
+
+module.exports = [
+  {
+    handle: 'meta',
+    options: {
+      logRequest: isDev,
+      sendResponseTime: isDev
+    }
+  },
+  {
+    handle: 'trace',
+    enable: !think.isCli,
+    options: {
+      debug: isDev
+    }
+  },
+  {
+    handle: 'payload',
+    options: {
+      keepExtensions: true,
+      limit: '5mb'
+    }
+  },
+  {
+    handle: 'router',
+    options: {}
+  },
+  'logic',
+  'controller'
+];
