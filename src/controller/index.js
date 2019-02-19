a43c559662ab7f46299849eff5660205bcf059f2
lizheming <i@imnerd.org>
2019-02-19 09:45:21 +0800

    optimize code

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,32 +2,8 @@ const vm = require('vm');
 const yaml = require('yaml');
 module.exports = class extends think.Controller {
   async indexAction() {
-    const {
-      GITHUB_SERVER,
-      GITHUB_TOKEN,
-      GITLAB_SERVER,
-      GITLAB_TOKEN,
-      BITBUCKET_SERVER,
-      BITBUCKET_TOKEN
-    } = process.env;
-
-    let service;
-    if (GITLAB_TOKEN) {
-      service = this.service('gitlab', {
-        server: GITLAB_SERVER,
-        token: GITLAB_TOKEN
-      });
-    } else if (BITBUCKET_TOKEN) {
-      service = this.service('bitbucket', {
-        server: BITBUCKET_SERVER,
-        token: BITBUCKET_TOKEN
-      });
-    } else {
-      service = this.service('github', {
-        server: GITHUB_SERVER,
-        token: GITHUB_TOKEN
-      });
-    }
+    const { name, ...opts } = this.config('vcs');
+    const service = this.service(name, opts);
 
     const resp = await service.run(this.post());
     if (think.isEmpty(resp)) {
@@ -41,9 +17,6 @@ module.exports = class extends think.Controller {
     }
     const docs = data.map(doc => yaml.stringify(doc)).join('\n---\n');
 
-    this.type = 'json';
-    this.body = JSON.stringify({
-      Data: docs
-    });
+    return this.json({ Data: docs });
   }
 };
